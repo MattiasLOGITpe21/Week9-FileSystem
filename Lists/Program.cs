887e030e6d17c62cc5e655f3aafb4b44603129ba
Opilane TTHK <opilane@tthk.ee>
2021-10-28 09:18:01 +0300

    File System

---
+++ Lists/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
